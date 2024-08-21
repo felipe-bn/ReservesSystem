08542bbce425f533ae88ff5c59b3d906054d095c
Felipe Padrós <felipe.padros@icloud.com>
2024-08-21 17:46:51 -0300

    1.0
    fisrt version of the Frontend, already have nothing, only basic page from React

--- System/src/main/java/com/example/System/Security/Jwt/JwtService.java
+++ System/src/main/java/com/example/System/Security/Jwt/JwtService.java
@@ -3,8 +3,6 @@ package com.example.System.Security.Jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
